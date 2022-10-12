1ad347d516779f6ddce16cf0144793577e58c1c5
Gustavo Antonio Silva Navarro <gustavo.silva.nav@gmail.com>
2022-10-12 19:24:36 -0300

    feat: Sqlite3 Was added to the project!

--- server/orm/models/connectionDB.js
+++ server/orm/models/connectionDB.js
@@ -1,18 +1,25 @@
 'use strict';
 const { Sequelize } = require('sequelize');
 
-const env = require('../utils/env/env');
+// const env = require('../utils/env/env');
 
-const sequelize = new Sequelize(env.dbName, env.dbUserName, env.dbPassword, {
-  host: env.dbHost,
-  dialect: env.dbDialect,
-  port: env.dbPort,
-  pool: {
-    max: 5,
-    min: 0,
-    acquire: 30000,
-    idle: 10000
-  }
+//* Connection DB for postgres, mysql and mariadb
+// const sequelize = new Sequelize(env.dbName, env.dbUserName, env.dbPassword, {
+//   host: env.dbHost,
+//   dialect: env.dbDialect,
+//   port: env.dbPort,
+//   pool: {
+//     max: 5,
+//     min: 0,
+//     acquire: 30000,
+//     idle: 10000
+//   }
+// });
+
+//* Connection for sqlite
+const sequelize = new Sequelize({
+  dialect: 'sqlite',
+  storage: './server/orm/db/messages_db.sqlite'
 });
 
 module.exports = sequelize;
